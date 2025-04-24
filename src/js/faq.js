1b96ee31e158841354cb5f67dc2aa7152d74fb2d
Artem Shvachov <shvachev91@gmail.com>
2025-04-24 23:18:25 +0300

    mobile first without arrow

---
+++ src/js/faq.js
@@ -0,0 +1,9 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const accordions = Array.from(
+  document.querySelectorAll('.accordion-container')
+);
+new Accordion(accordions, {
+  //   showMultiple: true,
+});
