bb6fad6b95d34f59e7fe560a3149f919e0883732
Elmira <xzrklf@gmail.com>
2025-04-29 18:02:06 +0300

    my projects

--- src/js/my_projects.js
+++ src/js/my_projects.js
@@ -1,66 +1,65 @@
 const projects = [
-     {
-    image: './img/Rectangle 10.jpg',
+  {
+    image: './img/rectangle_10.webp',
     stack: 'React, JavaScript, Node JS, Git',
-    name: 'power pulse webservice ',
+    name: 'power pulse webservice',
     link: ''
   },
   {
-    image: './img/Rectangle 7.jpg',
+    image: './img/rectangle_7.webp',
     stack: 'React, JavaScript, Node JS, Git',
     name: 'mimino website',
     link: ''
-    },
-   {
-    image: './img/Rectangle 8.jpg',
+  },
+  {
+    image: './img/rectangle_8.webp',
     stack: 'React, JavaScript, Node JS, Git',
     name: 'vyshyvanka vibes Landing Page',
     link: ''
   },
   {
-    image: './img/Rectangle 9.jpg',
+    image: './img/rectangle_9.webp',
     stack: 'React, JavaScript, Node JS, Git',
     name: 'green harvest online store',
     link: ''
-    },
-   {
-    image: './img/Rectangle 11.jpg',
+  },
+  {
+    image: './img/rectangle_11.webp',
     stack: 'React, JavaScript, Node JS, Git',
-    name: 'wallet webservice ',
+    name: 'wallet webservice',
     link: ''
   },
   {
-    image: './img/Rectangle 6.jpg',
+    image: './img/rectangle_6.webp',
     stack: 'React, JavaScript, Node JS, Git',
     name: 'chego jewelry website',
     link: ''
-    },
-   {
-    image: './img/Rectangle 4.jpg',
+  },
+  {
+    image: './img/rectangle_4.webp',
     stack: 'React, JavaScript, Node JS, Git',
-    name: 'energy flow webservice ',
+    name: 'energy flow webservice',
     link: ''
   },
   {
-    image: './img/Rectangle 5.jpg',
+    image: './img/rectangle_5.webp',
     stack: 'React, JavaScript, Node JS, Git',
     name: 'fruitbox online store',
     link: ''
-    },
-   {
-    image: './img/Rectangle 12.jpg',
+  },
+  {
+    image: './img/rectangle_12.webp',
     stack: 'React, JavaScript, Node JS, Git',
-    name: 'English excellence webservice ',
+    name: 'English excellence webservice',
     link: ''
   },
   {
-    image: './img/first screen 1.jpg',
+    image: './img/first_screen_1.webp',
     stack: 'React, JavaScript, Node JS, Git',
     name: 'starlight studio landing page',
     link: ''
-  },
-]
-
+  }
+];
 const projectList = document.querySelector('.my_projects-list');
 const loadMoreButton = document.getElementById('load-more');
 
@@ -70,22 +69,35 @@ const projectsPerClick = 3;
 function createMarkup(arr) {
   return arr.map(item => `
     <li class="my_projects-item">
-      <img src="${item.image}" alt="${item.name}" class="my_projects-img">
+      <picture>
+        <source
+          srcset="${item.image.replace('.webp', '-1x.webp')} 1x, ${item.image.replace('.webp', '-2x.webp')} 2x"
+          type="image/webp"
+        />
+        <img 
+          src="${item.image.replace('.webp', '-1x.webp')}" 
+          alt="${item.name}" 
+          class="my_projects-img" 
+          loading="lazy" 
+          width="320" 
+          height="184"
+        />
+      </picture>
       <div class="my_projects-info-container">
         <div class="my_projects-info">
           <p class="my_projects-item-stack">${item.stack}</p>
           <h3 class="my_projects-item-header">${item.name}</h3>
         </div>
-        <a href="${item.link}" target="_blank">
-          <button type="button" class="my_projects-button">VISIT  
-            <img class="my_projects-icon"  src="./img/Vector 24.svg" width="18" height="18">
-          </button>
+        <a href="${item.link}" target="_blank" class="my_projects-button">
+          VISIT
+          <svg class="my_projects-icon" width="24" height="24">
+            <use href="./img/icons/icons.svg#icon-diagonal-arrow"></use>
+          </svg>
         </a>
       </div>
     </li>
   `).join('');
 }
-
 function loadProjects() {
   const slice = projects.slice(projectsLoaded, projectsLoaded + projectsPerClick);
   projectList.insertAdjacentHTML('beforeend', createMarkup(slice));
